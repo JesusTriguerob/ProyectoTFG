04f24302def9d23b4097fe7e7f898c3de571ba8c
Jesus Triguero <jesustribeg@gmail.com>
2020-03-20 18:29:40 +0100

    Proyecto angular

---
+++ frontRuix/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Prueba titulo';
+
+  curso = 'Prueba para el desarrollo de Aularuix';
+  desarrollador = 'Jesus Triguero Begines';
+}
